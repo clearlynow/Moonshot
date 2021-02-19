6be26805e21fe013fdc216c262bf506e53c84b0f
clearlynow <59779189+clearlynow@users.noreply.github.com>
2021-02-18 21:44:19 -0700

    Initial Commit

---
+++ Moonshot2/Moonshot2App.swift
@@ -0,0 +1,17 @@
+//
+//  Moonshot2App.swift
+//  Moonshot2
+//
+//  Created by Alison Gorman on 2/18/21.
+//
+
+import SwiftUI
+
+@main
+struct Moonshot2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
